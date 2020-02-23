830b136f2f74edc1c1f84250d2efd2b1361c699f
Sawyer Fisher <sawyerkove@gmail.com>
2020-02-22 18:41:58 -0600

    completed MVP of project also added readme and gif

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Profile_Generator
+
+This project prompts the user with two questions: What is their GitHub username and what's their favorite color?
+Based off the user's input, a profile will be generated into a readable PDF file that displays the their github username, profile picture, a link to their github, a link to their blog, a breif bio, number of public repos, number of followers, number of people they follow, number of stars they've received, and their location.
+
+All this information comes from an axios call to the GitHub API. The input of their favorite color will generate the card to be that specific color.
+
+User Story-
+AS A product manager
+
+I WANT a developer profile generator
+
+SO THAT I can easily prepare reports for stakeholders
--- index.js
+++ index.js
@@ -1,29 +1,102 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const axios = require("axios");
 
-let questions = [
-    {
-        type: "input",
-        name: "favColor",
-        message: "What is your favorite color?"
+const pdf = require("html-pdf");
+
+inquirer.prompt(
+    [{
+        message: "What is your GitHub username?",
+        name: "username"
     },
     {
-        type: "input",
-        name: "username",
-        message: "What's your GitHub Username"
+        message: "What is your favorite color?",
+        name: "color"
     }
-]
-function init() {
-    inquirer.prompt(questions)
-        .then(function ({ username }) {
-            const queryURL = `https://api.github.com/users/${username}`;
+    ]).then(function (input) {
 
-            axios
-                .get(queryURL)
-                .then(function (res) {
-                    console.log(res.data)
-                })
-        }
-        )
-}
-init();
+        axios.get("https://api.github.com/users/" + input.username).then(function (response) {
+            console.log(response);
+            const color = input.color;
+
+            const username = input.username;
+            const image = response.data.avatar_url;
+
+            const location = response.data.location;
+            const gitHub = response.data.html_url;
+
+            const blog = response.data.blog;
+            const bio = response.data.bio;
+
+            const numRepos = response.data.public_repos;
+
+            const numFollowers = response.data.followers;
+            const numFollowing = response.data.following;
+
+            const reposURL = response.data.repos_url;
+
+            let numStars = 0;
+
+            axios.get(reposURL).then(function (repos) {
+                for (let repo of repos.data) {
+                    numStars += repo.stargazers_count;
+                }
+            });
+
+            let htmlContent = `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+                <title>Document</title>
+            </head>
+            <style>
+                .lead {
+                    font-size: 14pt;
+                }
+                .card {
+                    background-color: ${color};
+                }
+                .image {
+                    text-align: center;
+                    padding-bottom: 25px;
+                }
+            
+            </style>
+            <body>
+                <div class="jumbotron center">
+                    
+                    <h2 style="text-align: center;" class="display-5">GitHub Username: ${username} </h2>
+                    <hr class="my-4">
+                    <p class="lead">Bio: ${bio}</p>
+                    <p>More info can be found on their portfolio: <a href="${blog}" target="_blank"> Link Here</a></p>
+                    <p>Here is a link to their GitHub profile: <a href="${gitHub}" target="_blank"> Click Here</a></p>
+                  </div>
+                  <div class="image">
+                  <img src="${image}" width="20%" alt="Profile image"/>
+              </div>
+      
+                  <div class="card">
+                    <div class="card-body">
+                        <p>Location: ${location}</p>
+                        <p>Number of public repositories: ${numRepos}</p>
+                        <p>Number of followers: ${numFollowers}</p>
+                        <p>Number of following: ${numFollowing}</p>
+                        <p>Total number of stars: ${numStars}</p>
+                    </div>
+                  </div>    
+                 </body>
+            </html>`;
+
+            //Convert to pdf
+            const options = { "orientation": "portrait" };
+
+            pdf.create(htmlContent, options).toFile("./" + username + ".pdf", function (err, res) {
+                if (err) {
+                    console.log(err);
+                    return
+                }
+            });
+        });
+    });
